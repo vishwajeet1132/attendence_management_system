ac29bccbf79bfc3816f0835fd35ef4adef08189a
Vishwajeet Singh <vishu1132@gmail.com>
2024-09-11 19:57:59 +0530

    models modified to make unique constraint

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,HTTPException, status
 from .request_models import UserRequest
 from .database import SessionLocal
 from .models import User
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 
 app = FastAPI()
 
@@ -16,7 +17,13 @@ def get_db():
 
 @app.post("/user/")
 async def create_user(user: UserRequest,db: Session = Depends(get_db)):
-    new_user = User(type=user.type, full_name = user.full_name,username=user.full_name,email=user.email,password=user.password,submitted_by=user.submitted_by)
+    user_exist = db.query(User).filter(or_(User.username == user.username, User.email == user.email)).first()
+    if user_exist:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this username or email already exists."
+        )
+    new_user = User(type=user.type, full_name=user.full_name,username=user.username,email=user.email,password=user.password,submitted_by=user.submitted_by)
     db.add(new_user)
     db.commit()
     return user
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func,UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     type = Column(String)
     full_name = Column(String)
-    username = Column(String)
+    username = Column(String,unique=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     submitted_by = Column(String)
@@ -46,7 +46,7 @@ class Departments(Base):
     __tablename__ = "departments"
 
     id = Column(Integer, primary_key=True)
-    department_name = Column(String)
+    department_name = Column(String,unique=True)
     submitted_by = Column(String)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
@@ -60,3 +60,7 @@ class Students(Base):
     class_ = Column('class', String)
     submitted_by = Column(String)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    __table_args__ = (
+        UniqueConstraint('full_name', 'class', name='uix_full_name_class'),
+    )
