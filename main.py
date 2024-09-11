46f9485f197f41e41b98d013732e7868ec4c4b48
Vishwajeet Singh <vishu1132@gmail.com>
2024-09-11 20:25:15 +0530

    resopnse code for create user api corrected

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from .database import SessionLocal
 from .models import User
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -19,9 +20,10 @@ def get_db():
 async def create_user(user: UserRequest,db: Session = Depends(get_db)):
     user_exist = db.query(User).filter(or_(User.username == user.username, User.email == user.email)).first()
     if user_exist:
-        return HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User with this username or email already exists."
+        data = {"message":"User with this username or email already exists."}
+        return JSONResponse(
+            status_code=400,
+            content=data
         )
     new_user = User(type=user.type, full_name=user.full_name,username=user.username,email=user.email,password=user.password,submitted_by=user.submitted_by)
     db.add(new_user)
